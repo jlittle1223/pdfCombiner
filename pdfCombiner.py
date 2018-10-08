9d9fd0d39680e90674f41d8f99a0b643cba9e19e
Laptop92 <Laptop92@users.noreply.github.com>
2018-10-07 20:10:48 -0700

    Integrated Send2Trash, guaranteed unique result file

--- pdfCombiner.py
+++ pdfCombiner.py
@@ -1,38 +1,56 @@
 from PyPDF2 import PdfFileMerger
 import os
+from send2trash import send2trash
 
-files = os.listdir()
-pdfs = []
-for file in files:
-    if file[-4:] == ".pdf":
-        pdfs.append(file)
-
-numFiles = len(pdfs)
-
-assert numFiles % 2 == 0
-
-toMerge = []
-
-for offset in range(int(numFiles / 2)):
+try:
+    pdf_extension = ".pdf"
     
-    toMerge.append(pdfs[offset])
-    toMerge.append(pdfs[-(offset + 1)])
-
-
-merger = PdfFileMerger()
-read_handles = []
-
-for pdf in toMerge:
-    handle = open(pdf, 'rb')
-    merger.append(handle)
-    read_handles.append(handle)
-
-with open('result.pdf', 'wb') as fout:
-    merger.write(fout)
-    fout.close()
-	
-for handle in read_handles:
-    handle.close()
-
-for pdf in pdfs:
-    os.remove(pdf)
+    files = os.listdir()
+    pdfs = []
+    for file in files:
+        if file[-4:] == pdf_extension:
+            pdfs.append(file)
+    
+    numFiles = len(pdfs)
+    
+    assert numFiles % 2 == 0, "Error: Encountered {} .pdf files, need an even number".format(numFiles)
+    
+    toMerge = []
+    
+    for offset in range(int(numFiles / 2)):
+        toMerge.append(pdfs[offset])
+        toMerge.append(pdfs[-(offset + 1)])
+    
+    merger = PdfFileMerger()
+    read_handles = []
+    
+    for pdf in toMerge:
+        handle = open(pdf, 'rb')
+        merger.append(handle)
+        read_handles.append(handle)
+        
+    result_directory = "results"
+    result_prefix = "result"
+    result_filename = result_prefix+pdf_extension
+    result_path = os.path.join(result_directory, result_filename)
+    copy_index = 1
+    
+    os.makedirs(result_directory, exist_ok=True)
+    
+    while os.path.exists(result_path):
+        result_filename = result_prefix+"_"+str(copy_index)+pdf_extension
+        result_path = os.path.join(result_directory, result_filename)
+        copy_index += 1
+    
+    with open(result_path, 'wb') as fout:
+        merger.write(fout)
+        fout.close()
+    	
+    for handle in read_handles:
+        handle.close()
+    
+    for pdf in pdfs:
+        send2trash(pdf)
+except Exception as e:
+    print(e)
+    input()
